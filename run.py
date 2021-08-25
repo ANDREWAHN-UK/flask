738e64862f6ba3fe1339bc29ea40f414e08a44a0
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-08-25 14:34:46 +0000

    run.py - updated to use secret key;templates - updated contact.html to display flash message after form is submitted; updated git ignore file

--- run.py
+++ run.py
@@ -2,9 +2,10 @@ import os
 import json
 from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
-    import env 
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,7 +35,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print("Hello Monsignor Ahn")
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -48,4 +50,6 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
- 
+
+
+
